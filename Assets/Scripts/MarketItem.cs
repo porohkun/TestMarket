d959459600d97f3522c79f0c3892a496da81691b
Poroh <meta.poroh.kun@gmail.com>
2015-04-28 16:26:34 +0300

    buttons prefabs loading fix

--- Assets/Scripts/MarketItem.cs
+++ Assets/Scripts/MarketItem.cs
@@ -1,115 +1,19 @@
-﻿using System;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+[System.Serializable]
 public class MarketItem
 {
+    public string ID;
     public string Name;
     public int Price;
-    public string Model;
+    public Transform Model;
 
-    public MarketContent Content;
-    public MarketButton Button;
-
-    private MarketItem()
-    {
-
-    }
-    public static MarketItem[] Load()
-    {
-        return new MarketItem[15]
-        {
-            new MarketItem()
-            {
-                Name = "Белый ящик",
-                Price = 15,
-                Model = "white_box"
-            },
-            new MarketItem()
-            {
-                Name = "Синий бокс",
-                Price = 23,
-                Model = "blue_box"
-            },
-            new MarketItem()
-            {
-                Name = "Белошар",
-                Price = 37,
-                Model = "white_sphere"
-            },
-            new MarketItem()
-            {
-                Name = "Зелен короб",
-                Price = 11,
-                Model = "green_box"
-            },
-            new MarketItem()
-            {
-                Name = "Белая табла",
-                Price = 28,
-                Model = "white_cap"
-            },
-            new MarketItem()
-            {
-                Name = "Синяя таблетка",
-                Price = 14,
-                Model = "blue_cap"
-            },
-            new MarketItem()
-            {
-                Name = "Красная капсула",
-                Price = 34,
-                Model = "red_cap"
-            },
-            new MarketItem()
-            {
-                Name = "Зелен горох",
-                Price = 75,
-                Model = "green_cap"
-            },
-            new MarketItem()
-            {
-                Name = "Штучка",
-                Price = 31,
-                Model = "abstract1"
-            },
-            new MarketItem()
-            {
-                Name = "Фигень",
-                Price = 19,
-                Model = "abstract2"
-            },
-            new MarketItem()
-            {
-                Name = "Красный куб",
-                Price = 8,
-                Model = "red_box"
-            },
-            new MarketItem()
-            {
-                Name = "Абзац",
-                Price = 22,
-                Model = "abstract3"
-            },
-            new MarketItem()
-            {
-                Name = "Штуковина",
-                Price = 64,
-                Model = "abstract4"
-            },
-            new MarketItem()
-            {
-                Name = "Непонятка",
-                Price = 48,
-                Model = "abstract5"
-            },
-            new MarketItem()
-            {
-                Name = "Шуршурка",
-                Price = 100,
-                Model = "abstract6"
-            }
-        };
-    }
+    public MarketContent Content { get; set; }
+    public MarketButton Button { get; set; }
 }
--- Assets/Scripts/MarketManager.cs
+++ Assets/Scripts/MarketManager.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 
 public class MarketManager: MonoBehaviour
 {
-    MarketItem[] Contents;
-
     public CanvasGroup MarketMenu;
     public CanvasGroup MainMenu;
     public MenuManager MM;
@@ -14,36 +12,17 @@ public class MarketManager: MonoBehaviour
     public Text ShowName;
 
     public float OffsetStep = 10f;
-    public static Dictionary<string, Transform> Prefabs = new Dictionary<string, Transform>();
 
     public MarketButton BtnPrefab;
     public MarketContent CntPrefab;
     public Transform Grid;
 
-    public Transform white_box;
-    public Transform blue_box;
-    public Transform green_box;
-    public Transform red_box;
-    public Transform white_cap;
-    public Transform blue_cap;
-    public Transform green_cap;
-    public Transform red_cap;
-    public Transform white_sphere;
-    public Transform abstract1;
-    public Transform abstract2;
-    public Transform abstract3;
-    public Transform abstract4;
-    public Transform abstract5;
-    public Transform abstract6;
+    public MarketItem[] MarketItems = new MarketItem[0];
 
     void Start()
     {
-        LoadPrefabs();
-
-        Contents = MarketItem.Load();
-
         int offset = 0;
-        foreach (MarketItem item in Contents)
+        foreach (MarketItem item in MarketItems)
         {
             MarketContent content = (MarketContent)Instantiate(CntPrefab);
             content.transform.SetParent(transform);
@@ -62,25 +41,6 @@ public class MarketManager: MonoBehaviour
             item.Content = content;
             item.Button = button;
         }
-    }
-
-    void LoadPrefabs()
-    {
-        Prefabs.Add("white_box", white_box);
-        Prefabs.Add("blue_box", blue_box);
-        Prefabs.Add("green_box", green_box);
-        Prefabs.Add("red_box", red_box);
-        Prefabs.Add("white_cap", white_cap);
-        Prefabs.Add("blue_cap", blue_cap);
-        Prefabs.Add("green_cap", green_cap);
-        Prefabs.Add("red_cap", red_cap);
-        Prefabs.Add("white_sphere", white_sphere);
-        Prefabs.Add("abstract1", abstract1);
-        Prefabs.Add("abstract2", abstract2);
-        Prefabs.Add("abstract3", abstract3);
-        Prefabs.Add("abstract4", abstract4);
-        Prefabs.Add("abstract5", abstract5);
-        Prefabs.Add("abstract6", abstract6);
     }
 
     public void BuyItem(MarketItem item)
